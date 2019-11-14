8226b311d2055ac47d6d6ca23e29919f2303c6a7
hermesdiaz <57687563+hermesdiaz@users.noreply.github.com>
2019-11-13 21:07:34 -0500

    #7
    Se Crea  Script GameManager para controlar el estado del juego y los botones

---
+++ Space-up/Assets/scripts/GameManager1.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public delegate void GameDelegate();
+    public static event GameDelegate OnGameStarted;
+    public static event GameDelegate OnGameOverConfirmed;
+
+    public static GameManager Instance;
+
+    public GameObject startPage;
+    public GameObject gameOverPage;
+    public GameObject countdownPage;
+    public TextAlignment scoreText;
+
+    enum PageState
+    {
+        None,
+        Start,
+        GameOver,
+        Countdown
+    }
+    int score = 0;
+    bool gameOver = false;
+
+    public bool GameOver { get { return gameOver; } }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    void SetPageState(PageState state)
+    {
+        switch (state)
+        {
+            case PageState.None:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                countdownPage.SetActive(false);
+                break;
+            case PageState.Start:
+                startPage.SetActive(true);
+                gameOverPage.SetActive(false);
+                countdownPage.SetActive(false);
+                break;
+            case PageState.GameOver:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(true);
+                countdownPage.SetActive(false);
+                break;
+            case PageState.Countdown:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                countdownPage.SetActive(true);
+                break;
+        }
+    }
+
+    public void ConfirmGameOver()
+    {
+        //activated when replay button is hit
+    }
+
+    public void StartGame()
+    {
+        //activated when play button is hit
+    }
+}
