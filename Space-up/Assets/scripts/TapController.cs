10b35cc5c2d32b2ad9af17e0389a8a4b78a8b26b
hermesdiaz <57687563+hermesdiaz@users.noreply.github.com>
2019-11-22 10:11:08 -0500

    #18
    Se actualizo TapController, Game Manager y se creo el controlador de puntuacion llamado CountDownText

--- Space-up/Assets/scripts/TapController.cs
+++ Space-up/Assets/scripts/TapController.cs
@@ -2,53 +2,91 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
-[RequireComponent (typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class TapController : MonoBehaviour
 {
+
+    public delegate void PlayerDelegate();
+    public static event PlayerDelegate OnPlayerDied;
+    public static event PlayerDelegate OnPlayerScored;
+
     public float tapForce = 10;
     public float tiltSmooth = 5;
-        public Vector3 startPos;
+    public Vector3 startPos;
+    public AudioSource tapSound;
+    public AudioSource scoreSound;
+    public AudioSource dieSound;
 
-    Rigidbody2D rigidbody;
+    Rigidbody2D rigidBody;
     Quaternion downRotation;
     Quaternion forwardRotation;
 
-    private void Start()
-    {
-        rigidbody = GetComponent<Rigidbody2D>();
-        downRotation = Quaternion.Euler(0, 0, -90);
-        forwardRotation = Quaternion.Euler(0, 0, 35);
+    GameManager game;
+    TrailRenderer trail;
 
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        downRotation = Quaternion.Euler(0, 0, -100);
+        forwardRotation = Quaternion.Euler(0, 0, 40);
+        game = GameManager.Instance;
+        rigidBody.simulated = false;
+        //trail = GetComponent<TrailRenderer>();
+        //trail.sortingOrder = 20; 
     }
 
-    private void Update()
+    void OnEnable()
     {
-         if (Input.GetMouseButtonDown(0)) {
+        GameManager.OnGameStarted += OnGameStarted;
+        GameManager.OnGameOverConfirmed += OnGameOverConfirmed;
+    }
+
+    void OnDisable()
+    {
+        GameManager.OnGameStarted -= OnGameStarted;
+        GameManager.OnGameOverConfirmed -= OnGameOverConfirmed;
+    }
+
+    void OnGameStarted()
+    {
+        rigidBody.velocity = Vector3.zero;
+        rigidBody.simulated = true;
+    }
+
+    void OnGameOverConfirmed()
+    {
+        transform.localPosition = startPos;
+        transform.rotation = Quaternion.identity;
+    }
+
+    void Update()
+    {
+        if (game.GameOver) return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            rigidBody.velocity = Vector2.zero;
             transform.rotation = forwardRotation;
-            rigidbody.velocity = Vector3.zero;
-            rigidbody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
-            
+            rigidBody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+            tapSound.Play();
         }
-        transform.rotation = Quaternion.Lerp(transform.rotation, downRotation,tiltSmooth * Time.deltaTime);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.tag == "ScoreZone") {
-            //register a score event
-            //play a sound
+        if (col.gameObject.tag == "ScoreZone")
+        {
+            OnPlayerScored();
+            scoreSound.Play();
         }
         if (col.gameObject.tag == "DeadZone")
         {
-            rigidbody.simulated = false;
-            //register a dead event
-            //play a sound
+            rigidBody.simulated = false;
+            OnPlayerDied();
+            dieSound.Play();
         }
     }
-
-
-
 
 }
