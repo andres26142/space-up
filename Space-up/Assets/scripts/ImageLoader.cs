7e86a19f9451a0af510ed4d530bd9f68893cfad0
andres26142 <56303578+andres26142@users.noreply.github.com>
2020-02-15 11:46:18 -0500

    Fix #28
    Se creo un objeto de tipo Plane para cargar la imagen obtenida del sitio web en este

--- Space-up/Assets/scripts/ImageLoader.cs
+++ Space-up/Assets/scripts/ImageLoader.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class ImageLoader : MonoBehaviour
 {
     public string url = "https://cdn.eso.org/images/screen/potw2004a.jpg";
-    public Sprite thisSprite;
+    public Renderer thisRenderer;
 
     // automatically called when game started
     void Start()
@@ -15,6 +15,7 @@ public class ImageLoader : MonoBehaviour
 
         // the following will be called even before the load finished
         //thisSprite.Sprite = Color.red;
+        thisRenderer.material.color = Color.red;
     }
 
     // this section will be run independently
@@ -23,10 +24,10 @@ public class ImageLoader : MonoBehaviour
         Debug.Log("Loading ....");
         WWW wwwLoader = new WWW(url);   // create WWW object pointing to the url
         yield return wwwLoader;         // start loading whatever in that url ( delay happens here )
-        thisSprite=Sprite.Create(wwwLoader.texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f,0.5f),100);;
+        //thisSprite=Sprite.Create(wwwLoader.texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f,0.5f),100);;
         
         Debug.Log("Loaded");
-        //thisRenderer.material.color = Color.white;              // set white
-        //thisRenderer.material.mainTexture = wwwLoader.texture;  // set loaded image
+        thisRenderer.material.color = Color.white;              // set white
+        thisRenderer.material.mainTexture = wwwLoader.texture;  // set loaded image
     }
 }
