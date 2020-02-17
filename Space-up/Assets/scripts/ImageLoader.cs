9e62c44245a730186e6ceff0436d645d7b517257
andres26142 <andres26142@gmail.com>
2020-02-17 06:39:43 -0500

    Fix #29

--- Space-up/Assets/scripts/ImageLoader.cs
+++ Space-up/Assets/scripts/ImageLoader.cs
@@ -2,12 +2,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ImageLoader : MonoBehaviour
 {
     public string url = "https://cdn.eso.org/images/screen/potw2004a.jpg";
     public Renderer thisRenderer;
-
+    
     // automatically called when game started
     void Start()
     {
@@ -15,7 +16,7 @@ public class ImageLoader : MonoBehaviour
 
         // the following will be called even before the load finished
         //thisSprite.Sprite = Color.red;
-        thisRenderer.material.color = Color.red;
+        //thisRenderer.material.mainTexture = Resources.Load<Texture2D>("Backgrounds/eso1740a");
     }
 
     // this section will be run independently
@@ -23,11 +24,19 @@ public class ImageLoader : MonoBehaviour
     {
         Debug.Log("Loading ....");
         WWW wwwLoader = new WWW(url);   // create WWW object pointing to the url
-        yield return wwwLoader;         // start loading whatever in that url ( delay happens here )
+                // start loading whatever in that url ( delay happens here )
         //thisSprite=Sprite.Create(wwwLoader.texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f,0.5f),100);;
-        
+        yield return wwwLoader;
         Debug.Log("Loaded");
-        thisRenderer.material.color = Color.white;              // set white
-        thisRenderer.material.mainTexture = wwwLoader.texture;  // set loaded image
+        thisRenderer.material.color = Color.white;     // set white
+        if(wwwLoader.error != null){
+            thisRenderer.material.mainTexture = Resources.Load<Texture2D>("backgrounds/eso1740a"); 
+            Debug.Log("exc");
+        }
+        else{
+            thisRenderer.material.mainTexture = wwwLoader.texture;  // set loaded image
+        }
+        
+        
     }
 }
